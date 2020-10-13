a51604215bed5a610667910c517694f273da2711
Charity Brown <charitybrown@Charitys-MacBook-Pro.local>
2020-10-12 21:57:40 -0500

    Finished json to java function

--- DFOptimizerApp/app/src/main/java/com/example/dfoptimizerapp/HttpConnect.java
+++ DFOptimizerApp/app/src/main/java/com/example/dfoptimizerapp/HttpConnect.java
@@ -35,9 +35,6 @@ public class HttpConnect extends AppCompatActivity {
     private static final String TAG= HttpConnect.class.getName();
     private RequestQueue mRequestQueue;
     private StringRequest stringRequest;
-    //private String url="http://73.82.159.9:5000/";
-    //ArrayList<String> myList = (ArrayList<String>) getIntent().getSerializableExtra("selectedPlayers");
-//private String url= "https://run.mocky.io/v3/9c07d6d8-1ca2-4ca9-a2d2-fe104731e71c";
 
 
     @Override
@@ -51,8 +48,6 @@ public class HttpConnect extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                Toast.makeText(getApplicationContext(), "Sending http request", Toast.LENGTH_SHORT).show();
                 SendRequestAndPrintResponse();
             }
         });
@@ -68,9 +63,6 @@ public class HttpConnect extends AppCompatActivity {
         int i;
         int j;
         ArrayList<String> myList = (ArrayList<String>) getIntent().getSerializableExtra("selectedPlayers");
-        //ArrayList<String> myList = new ArrayList<String>();
-        //myList.add("Lebron James");
-        //myList.add("Kawhi Leonard");
         ArrayList<String> newList = new ArrayList<String>();
         String word;
         String newWord = "";
@@ -117,20 +109,13 @@ public class HttpConnect extends AppCompatActivity {
 
 
 
-
-
-    //Textview to Display optimized lineup
-        TextView txtView = (TextView) findViewById(R.id.textView3);
-
         mRequestQueue = Volley.newRequestQueue(this);
 
         stringRequest = new StringRequest(Request.Method.GET, finalURL, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 Log.i(TAG, "Response: " + response.toString());
-                Toast.makeText(getApplicationContext(), "Response: " + response.toString(), Toast.LENGTH_SHORT).show();
-                txtView.setText(response.toString());
-                //String testResponse = "{output: [{players: Lebron James}, {score: 42}]}";
+                Toast.makeText(getApplicationContext(), "Generating Optimized Lineup... " , Toast.LENGTH_SHORT).show();
                 ParseReceive(response.toString());
 
             }
@@ -146,7 +131,8 @@ public class HttpConnect extends AppCompatActivity {
 
 
     public void ParseReceive(String jsonStr) {
-
+        TextView txtView = (TextView) findViewById(R.id.textView3);
+        txtView.setText(" ");
         JSONArray jsonarray = null;
         try {
             jsonarray = new JSONArray(jsonStr);
@@ -156,8 +142,9 @@ public class HttpConnect extends AppCompatActivity {
                 JSONObject jsonobject = jsonarray.getJSONObject(i);
                 String player = jsonobject.getString("player");
                 String score = jsonobject.getString("score");
-                System.out.println(player);
-                System.out.println(score);
+                System.out.println(player + " " + score);
+                txtView.append(player + " " + score);
+                txtView.append("\n");
             }
         } catch (JSONException e) {
                 e.printStackTrace();
@@ -173,51 +160,3 @@ public class HttpConnect extends AppCompatActivity {
 
 
 
-// In other words : print("{output : [{players:" + player(0) + ", score:" + player(1) + "}]}")
-    /*
-        String input = "{output : [{players:LebronJames, score:20}]}";
-    String input2 = "{output : [{players:LebronJames, score:20}]}";
-
-try {
-    JSONObject obj = new JSONObject(input);
-
-    ArrayList<String> list = new ArrayList<String>();
-    ArrayList<String> scoreList = new ArrayList<String>();
-    JSONArray array = obj.getJSONArray("output");
-    for (int i = 0; i < array.length(); i++) {
-        list.add(array.getJSONObject(i).getString("players"));
-        scoreList.add(array.getJSONObject(i).getString("score"));
-
-    }
-for(int j = 0; j < array.length(); j++){
-    System.out.println(list.get(j) + " " + scoreList.get(j));
-}
-
-}
-catch(JSONException e) {
-    Log.e(TAG, "unexpected JSON exception", e);
-    // Do something to recover ... or kill the app.
-
-        }
-
-*/
-
-    /*
-        try {
-            JSONObject obj = new JSONObject(received);
-            String playerName = obj.getJSONObject("output").getString("players");
-            System.out.println(playerName);
-            //JSONArray arr = obj.getJSONArray("posts");
-            /*for (int i = 0; i < arr.length(); i++)
-            {
-                String post_id = arr.getJSONObject(i).getString("post_id");
-
-            }
-            */
-
-/*
-        } catch (JSONException e) {
-            Log.e(TAG, "unexpected JSON exception", e);
-            // Do something to recover ... or kill the app.
-        }
-*/

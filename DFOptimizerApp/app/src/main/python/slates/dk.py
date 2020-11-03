654c8d3f4d704b4d712b285349428057fa677dfd
Andrew Johnson <apjohnson12@crimson.ua.edu>
2020-11-03 03:12:52 -0600

    more testing with api, time zone calculation functions to come. Issue with type of contest and draftable players returned

--- DFOptimizerApp/app/src/main/python/slates/dk.py
+++ DFOptimizerApp/app/src/main/python/slates/dk.py
@@ -1,17 +1,22 @@
 from draft_kings.data import Sport
 from draft_kings.client import contests
 from draft_kings.client import available_players
-
+from draft_kings.client import draftables
+from draft_kings.client import draft_group_details
 
 contests = contests(sport=Sport.NFL)
 
 for label in contests["contests"]:
-    if not str(label["starts_at"]).startswith("2020-11-01"):
+    if not str(label["starts_at"]).startswith("2020-11-08"):
         continue
+    print(label["starts_at"])
     id = label["draft_group_id"]
+    print(id+1)
     break
-print(id)
 
-print(available_players(draft_group_id=id))
+
+# print(available_players(draft_group_id=id))
+print(draftables(id))
+# print(draft_group_details(id))
 
 # print(available_players(draft_group_id=11513))

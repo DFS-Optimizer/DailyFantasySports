ddad718ef33bc9d40ba091d4743e192fc99ce36d
Andrew Johnson <apjohnson12@crimson.ua.edu>
2020-11-07 16:09:26 -0600

    added in automation for NBA DK slate grabbing. Backend now grabs sundays slate every day and has it ready to display on the front end when requested.

--- DFOptimizerApp/app/src/main/python/slates/dk.py
+++ DFOptimizerApp/app/src/main/python/slates/dk.py
@@ -1,22 +1,84 @@
+import pandas as pd
+import datetime
 from draft_kings.data import Sport
 from draft_kings.client import contests
 from draft_kings.client import available_players
 from draft_kings.client import draftables
 from draft_kings.client import draft_group_details
 
+
 contests = contests(sport=Sport.NFL)
 
-for label in contests["contests"]:
-    if not str(label["starts_at"]).startswith("2020-11-08"):
-        continue
-    print(label["starts_at"])
-    id = label["draft_group_id"]
-    print(id+1)
-    break
+
+def changedate():
+    day = datetime.datetime.now()
+    while day.weekday() != 6:
+        day+=datetime.timedelta(days=1)
+    return day.strftime("%Y-%m-%d")
 
 
-# print(available_players(draft_group_id=id))
-print(draftables(id))
-# print(draft_group_details(id))
 
-# print(available_players(draft_group_id=11513))
+# print(changedate())
+
+
+def updatenflDKslate():
+
+
+    # contests = contests(sport=Sport.NFL)
+    df = pd.DataFrame()
+
+
+    for label in contests["contests"]:
+        if not str(label["starts_at"]).startswith(changedate()):
+            continue
+        # print(label["starts_at"])
+        id = label["draft_group_id"]
+        break
+        
+
+    players = draftables(id)
+
+    playerName = [x['names']['display'] for x in players['draftables']]
+    sal = [x['salary'] for x in players['draftables']]
+    pos = [x['position'] for x in players['draftables']]
+    team =  [x['team_abbreviation'] for x in players['draftables']]
+    opp = [x['competition']['name'] for x in players['draftables']]
+
+    index = 0
+    while index < len(playerName):
+        
+        if index > 0 and playerName[index] == playerName[index-1]:
+            del playerName[index]
+            del sal[index]
+            del pos[index]
+            del team[index]
+            del opp[index]
+
+        else:
+            temp = opp[index].split()
+            if temp[0] == team[index]:
+                opp[index] = temp[-1]
+            else:
+                opp[index] = temp[0]
+            index+=1
+
+    df['playerName'] = playerName
+    df['sal'] = sal
+    df['pos'] = pos
+    df['team'] = team
+    df['opp'] = opp
+
+
+    df['proj'] = [0 for _ in range(len(playerName))]
+
+    df.to_csv('slates/NFLslateDK.csv', index=False)
+
+
+
+
+# file = open("draftables.txt", "w")
+# print(available_players(draft_group_id=41096))
+# print(str(draftables(41096))[:20000])
+# # print(draft_group_details(id))
+
+# print(available_players(draft_group_id=41391))

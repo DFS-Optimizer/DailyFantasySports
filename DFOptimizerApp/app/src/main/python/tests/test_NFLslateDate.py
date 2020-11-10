b7b3a184e90fca5592c17afa04aaeefef57faa89
Andrew Johnson <apjohnson12@crimson.ua.edu>
2020-11-09 20:37:25 -0600

    NFL slate sunday date check test

---
+++ DFOptimizerApp/app/src/main/python/tests/test_NFLslateDate.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+import os, sys
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+import datetime
+from slates import dk
+
+class MainNFLDKTest(TestCase):
+    def test_nfl_sunday(self):
+        x = datetime.datetime(2020, 11, 9, 20, 27, 15, 414721)
+        x_sunday = datetime.datetime(2020, 11, 15, 20, 27, 15, 414721)
+        checking = False
+        x_new = changedate_nfl
+        if x_new == x_sunday:
+            checking = True
+        self.assertEquals(True, checking)
+

69b757fb789787775c02a65de18f4e7240974cfd
Andrew Johnson <apjohnson12@crimson.ua.edu>
2020-11-19 01:18:25 -0600

    messing with logo and colors of app, trying to make it look a little more friendly

---
+++ DFOptimizerApp/app/src/main/python/tests/test.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+import os, sys
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+import datetime
+from slates import dk
+
+
+x = datetime.datetime(2020, 11, 9, 20, 27, 15, 414721)
+x_sunday = datetime.datetime(2020, 11, 15, 20, 27, 15, 414721)
+print(x_sunday)

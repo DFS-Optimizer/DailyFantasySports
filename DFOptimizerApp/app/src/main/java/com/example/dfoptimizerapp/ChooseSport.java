05368d7f8787954532dd7ffb758064afb1151245
Andrew Johnson <apjohnson12@crimson.ua.edu>
2020-11-28 03:06:36 -0600

    changed interface on ChooseSport page, just buttons there now. Left old code commented in case we need to go back for some reason. Struggling to style the player list part of the app, not sure how to change colors and position

--- DFOptimizerApp/app/src/main/java/com/example/dfoptimizerapp/ChooseSport.java
+++ DFOptimizerApp/app/src/main/java/com/example/dfoptimizerapp/ChooseSport.java
@@ -14,40 +14,55 @@ public class ChooseSport extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_sport);
 
-        final Switch nbaSwitch = (Switch) findViewById(R.id.nbaSwitch);
-        final Switch nflSwitch = (Switch) findViewById(R.id.nflSwitch);
-        final Switch mlbSwitch = (Switch) findViewById(R.id.mlbSwitch);
-        final Button continueBtn = (Button) findViewById(R.id.continueBtn);
+//        final Switch nbaSwitch = (Switch) findViewById(R.id.nbaSwitch);
+//        final Switch nflSwitch = (Switch) findViewById(R.id.nflSwitch);
+//        final Switch mlbSwitch = (Switch) findViewById(R.id.mlbSwitch);
+        //final Button continueBtn = (Button) findViewById(R.id.continueBtn);
+        final Button nbaButton = (Button) findViewById(R.id.nbaButton);
+        final Button nflButton = (Button) findViewById(R.id.nflButton);
+        final Button mlbButton = (Button) findViewById(R.id.mlbButton);
         final int site = getIntent().getIntExtra("siteChoice",1);
 
-        nbaSwitch.setOnCheckedChangeListener((v, b) ->{
-            nflSwitch.setChecked(false);
-            mlbSwitch.setChecked(false);
-            nbaSwitch.setChecked(b);
-        });
-        nflSwitch.setOnCheckedChangeListener((v, b) ->{
-            nbaSwitch.setChecked(false);
-            mlbSwitch.setChecked(false);
-            nflSwitch.setChecked(b);
-        });
-        mlbSwitch.setOnCheckedChangeListener((v,b) ->{
-            nbaSwitch.setChecked(false);
-            nflSwitch.setChecked(false);
-            mlbSwitch.setChecked(b);
-        });
-        continueBtn.setOnClickListener((v) -> {
-            if(nbaSwitch.isChecked()) {
-                new GetSlate(v,site,1,this);
-            }
-            else if(nflSwitch.isChecked()){
-                new GetSlate(v,site,2, this);
-            }
-            else if(mlbSwitch.isChecked())
-            {
-                new GetSlate(v,site,3,this);
-            }
 
+        nflButton.setOnClickListener((v) -> {
+            new GetSlate(v, site, 2, this);
         });
+
+        nbaButton.setOnClickListener((v) -> {
+            new GetSlate(v, site, 1, this);
+        });
+
+        mlbButton.setOnClickListener((v) -> {
+            new GetSlate(v, site, 3, this);
+        });
+//        nbaSwitch.setOnCheckedChangeListener((v, b) ->{
+//            nflSwitch.setChecked(false);
+//            mlbSwitch.setChecked(false);
+//            nbaSwitch.setChecked(b);
+//        });
+//        nflSwitch.setOnCheckedChangeListener((v, b) ->{
+//            nbaSwitch.setChecked(false);
+//            mlbSwitch.setChecked(false);
+//            nflSwitch.setChecked(b);
+//        });
+//        mlbSwitch.setOnCheckedChangeListener((v,b) ->{
+//            nbaSwitch.setChecked(false);
+//            nflSwitch.setChecked(false);
+//            mlbSwitch.setChecked(b);
+//        });
+//        continueBtn.setOnClickListener((v) -> {
+//            if(nbaSwitch.isChecked()) {
+//                new GetSlate(v,site,1,this);
+//            }
+//            else if(nflSwitch.isChecked()){
+//                new GetSlate(v,site,2, this);
+//            }
+//            else if(mlbSwitch.isChecked())
+//            {
+//                new GetSlate(v,site,3,this);
+//            }
+//
+//        });
     }
 
 
